a1f98962e7303872e16a6f1854f0cc2f1260c061
qwabena-proxy <qwabenamike91@gmail.com>
2024-06-11 20:22:48 +0100

    Authentication system initial commit

---
+++ backend/authenticationSystem/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class RegistrationForm(UserCreationForm):
+    email= forms.EmailField(required= True)
+    firstName= forms.CharField(required= True)
+    lastName= forms.CharField(required= True)
+    
+    class Meta:
+        model= User
+        fields= ['firstName', 'lastName', 'username', 'email', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    # username= forms.CharField(required= True)
+
+    # class Meta:
+
+    
+
+    def clean_email(self):
+        email= self.cleaned_data.get('email')
+        if User.objects.filter(email= email).exists():
+            raise ValidationError('An account with this email address already exists')
+        return email
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Apps
+    'authenticationSystem',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +122,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS= [BASE_DIR/ 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+mail= os.environ.get('MAIL')
+password= os.environ.get('PASSWORD')
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER= mail
+EMAIL_HOST_PASSWORD= password
+EMAIL_USE_TLS= True
+
+LOGIN_REDIRECT_URL='/' # for redirecting users to index page after login
