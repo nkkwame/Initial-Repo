f9cea43210953effeec2f86a11d6749db89a1d24
qwabena-proxy <qwabenamike91@gmail.com>
2024-06-16 20:41:02 +0100

    Pulling  update from origin/authsystem branch

--- backend/authenticationSystem/views.py
+++ backend/authenticationSystem/views.py
@@ -1,17 +1,17 @@
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
 # from django.contrib.auth.decorators import login_required
+from django.utils.encoding import force_bytes, force_str
+from django.template.loader import render_to_string
 from django.contrib.auth.models import User, auth 
 # from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from django.contrib import messages
 from django.urls import reverse
 from .tokensGenerator import *
-from .forms import *
 from .models import *
+from .forms import *
 
 # Create your views here.
 # @login_required(login_url='login')
