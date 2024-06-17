33a8a20d256f62391cc2d4e27fdf552f9a82881e
qwabena-proxy <qwabenamike91@gmail.com>
2024-06-17 16:52:43 +0100

    Account and Password tokens expiry bug fixed 100%

--- backend/authenticationSystem/models.py
+++ backend/authenticationSystem/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+#Token model
+class TokensModel(models.Model):
+    token= models.CharField(blank=False, null=False, max_length= 999999999999)
+    timestamp= models.DateTimeField(blank=False, null=False, auto_now_add= True)
+    user_id= models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.user_id} ---> {self.timestamp}'
